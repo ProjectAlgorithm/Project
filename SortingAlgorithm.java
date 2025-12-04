02fd1d1ba524c29b04101023f48081db665e95e7
israhasan26 <israhasan602@gmail.com>
2025-12-04 18:47:49 +0200

    Update SortingAlgorithm.java

--- SortingAlgorithm.java
+++ SortingAlgorithm.java
@@ -90,9 +90,42 @@ public class SortingAlgorithm {
        }       
     }
  }
-    
-
-    
      
+    //quick sort
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+        quickSort(arr, low, pi -1);
+        quickSort(arr, pi + 1, high);
+    }
+   }
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low -1;
+        for (int j = low; j <= high -1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+ static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    // Randomized QuickSort
+    static void random(int arr[],int low,int high)
+{
+        Random rand= new Random();
+        int pivot = rand.nextInt(high-low)+low;
+        int temp1=arr[pivot];
+        arr[pivot]=arr[high];
+        arr[high]=temp1;
+   }
 }
+
 
