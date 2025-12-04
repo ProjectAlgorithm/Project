267f0ab956d6a8d345cbfc6ef4718d20763dd106
Fatima Naser 🩷 <fatimakarajah55@gmail.com>
2025-12-04 18:46:39 +0200

    Add files via upload
    main class

---
+++ AlgorithmProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.algorithmproject;
+
+/**
+ *
+ * @author USER
+ */
+public class AlgorithmProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
